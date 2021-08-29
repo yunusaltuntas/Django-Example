e6f02b5775b732f5b38df64c96eddaa56a0ff515
yunus <altuntasyunus50@gmail.com>
2021-08-29 17:46:57 +0300

    html page

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    my_dict = {"insert_me": "hello world"}
+    return render(request, "first_app/index.html", context=my_dict)
